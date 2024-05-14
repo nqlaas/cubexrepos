7e955ca73a00aacd8bbe4ef454baeea9921631f0
nqlaas <niklaseberhoefer@gmail.com>
2024-05-14 11:29:51 +0200

    Jetzt stimmts wirklich

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'sebernsql1' );
+define( 'DB_NAME', 'cubex' );
 
 /** Database username */
-define( 'DB_USER', 'sebernsql1' );
+define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', '*EDL5fk!sQF5KC#Q#qQn' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
